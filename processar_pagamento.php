a771fe9be228b21ce82bb9c73f8eb69d1da240bc
Itamar Lourenco <itamar.developer@gmail.com>
2019-12-07 12:01:03 -0300

    Update processar_pagamento.php

--- processar_pagamento.php
+++ processar_pagamento.php
@@ -1,4 +1,8 @@
-<?php  
+
+<?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 
 	$token = $_POST["token"];
 	$paymentMethodId = $_POST["paymentMethodId"];
