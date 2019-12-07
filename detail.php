dbc5efbe100a7b8d2d196265aca868992b9dee58
Rodrigo <ra@elevenfinancial.com>
2019-12-07 12:42:35 -0300

    Inclusão de formulários com valores recebidos por post

--- detail.php
+++ detail.php
@@ -134,11 +134,55 @@
                         </div>
                         <div class="as-producttile-info" style="float:left;min-height: 168px;">
                             <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
-                              
-                            <!--O FORMULARIO VAI AQUI -->
-                               
-                        </div>                  
-                </div>
+                                <form action="/processar_pagamento.php" method="post" id="pay" name="pay" >
+                                    <fieldset>
+                                        <ul>
+                                            <li>
+                                                <label for="email">Email</label>
+                                                <input type="email" id="email" name="email" value="test_user_19653727@testuser.com"  placeholder="your email"/>
+                                            </li>
+                                            <li>
+                                                <label for="cardNumber">Credit card number:</label>
+                                                <input type="text" id="cardNumber" data-checkout="cardNumber" placeholder="4509 9535 6623 3704" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off 
+                                                value="4509953566233704"/>
+                                            </li>
+                                            <li>
+                                                <label for="securityCode">Security code:</label>
+                                                <input type="text" id="securityCode" data-checkout="securityCode" placeholder="123" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off />
+                                            </li>
+                                            <li>
+                                                <label for="cardExpirationMonth">Expiration month:</label>
+                                                <input type="text" id="cardExpirationMonth" data-checkout="cardExpirationMonth" placeholder="11" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off />
+                                            </li>
+                                            <li>
+                                                <label for="cardExpirationYear">Expiration year:</label>
+                                                <input type="text" id="cardExpirationYear" data-checkout="cardExpirationYear" placeholder="2025" onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off />
+                                            </li>
+                                            <li>
+                                                <label for="cardholderName">Card holder name:</label>
+                                                <input type="text" id="cardholderName" data-checkout="cardholderName" placeholder="APRO" />
+                                            </li>
+                                            <li>
+                                                <label for="docType">Document type:</label>
+                                                <select id="docType" data-checkout="docType"></select>
+                                            </li>
+                                            <li>
+                                                <label for="docNumber">Document number:</label>
+                                                <input type="text" id="docNumber" data-checkout="docNumber" placeholder="19119119100" value="19119119100" />
+                                            </li>
+                                            <li>
+                                               <label for="installments">Installments:</label>
+                                               <select id="installments" class="form-control" name="installments"></select>
+                                          </li>
+                                        </ul>
+                                        <input type="hidden" name="amount" id="amount" value="<?php echo $_POST['price']; ?>" />
+                                        <input type="hidden" name="description" value="<?php echo $_POST['title']; ?>" />
+                                        <input type="hidden" name="paymentMethodId" />
+                                        <input type="submit" value="Pay!" />
+                                    </fieldset>
+                                </form>                              
+                            </div>                  
+                        </div>
             </div>
         </div>
     </div>
