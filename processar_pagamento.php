bc5a6efb331f87ec3b675213c844108e8ba10631
Itamar Lourenco <itamar.developer@gmail.com>
2019-12-07 12:15:25 -0300

    Update processar_pagamento.php

--- processar_pagamento.php
+++ processar_pagamento.php
@@ -3,6 +3,8 @@
 	ini_set('display_errors', 1);
 	error_reporting(-1);
 
+print(getcwd());
+
 	echo dirname(__FILE__) . '/vendor/autoload.php';
 
 // 	require_once dirname(__FILE__) . '/vendor/autoload.php';
