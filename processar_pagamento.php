f009f988865c029050af2004c07d4abdeaa0acc5
Itamar Lourenco <itamar.developer@gmail.com>
2019-12-07 12:31:24 -0300

    Update processar_pagamento.php

--- processar_pagamento.php
+++ processar_pagamento.php
@@ -4,32 +4,30 @@
 	error_reporting(-1);
 
 
-print_r(file_exists(dirname(__FILE__) . '/vendor/autoload.php'));
-	echo dirname(__FILE__) . '/vendor/autoload.php';
 
  	require_once dirname(__FILE__) . '/vendor/autoload.php';
-// 	$token = $_POST["token"];
-// 	$paymentMethodId = $_POST["paymentMethodId"];
-// 	$description = $_POST["description"];
-// 	$installments = $_POST["installments"];
+	$token = $_POST["token"];
+	$paymentMethodId = $_POST["paymentMethodId"];
+	$description = $_POST["description"];
+	$installments = $_POST["installments"];
 
 
 
-// 	MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
-// 	//...
-// 	$payment = new MercadoPago\Payment();
-// 	$payment->transaction_amount = 181;
-// 	$payment->token = $token;
-// 	$payment->description = $description;
-// 	$payment->installments = $installments;
-// 	$payment->payment_method_id = $paymentMethodId;
-// 	$payment->payer = array(
-// 		"email" => "itamar.ilourenco@gmail.com"
-// 	);
-// 	// Save and posting the payment
-// 	$payment->save();
-// 	//...
-// 	// Print the payment status
-// 	echo $payment->status;
-// 	//...
+	MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+	//...
+	$payment = new MercadoPago\Payment();
+	$payment->transaction_amount = 181;
+	$payment->token = $token;
+	$payment->description = $description;
+	$payment->installments = $installments;
+	$payment->payment_method_id = $paymentMethodId;
+	$payment->payer = array(
+		"email" => "itamar.ilourenco@gmail.com"
+	);
+	// Save and posting the payment
+	$payment->save();
+	//...
+	// Print the payment status
+	echo $payment->status;
+	//...
 ?>
